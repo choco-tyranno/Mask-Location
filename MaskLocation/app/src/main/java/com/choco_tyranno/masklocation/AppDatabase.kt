295eb08022dff426e935651f8a61cf077dd330c0
choco-tyranno <choco.tyranno@gmail.com>
2021-09-23 20:07:09 +0900

    chore : change java version 8-> 11.

---
+++ MaskLocation/app/src/main/java/com/choco_tyranno/masklocation/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.choco_tyranno.masklocation
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+}
