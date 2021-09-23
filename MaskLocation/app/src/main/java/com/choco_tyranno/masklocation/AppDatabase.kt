596f0779c3158749403c5ea6b6f89c62d771cccb
choco-tyranno <choco.tyranno@gmail.com>
2021-09-24 07:17:11 +0900

    chore : rename project name MaskLocation to StudyPjtKotlinA1.

--- MaskLocation/app/src/main/java/com/choco_tyranno/masklocation/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.choco_tyranno.masklocation
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Todo::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun todoDao(): TodoDao
-}
