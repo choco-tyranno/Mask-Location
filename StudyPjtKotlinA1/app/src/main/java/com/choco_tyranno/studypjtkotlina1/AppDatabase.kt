98f479ac3ee6c43cd80451f4bf71bd500118e509
choco-tyranno <choco.tyranno@gmail.com>
2021-09-24 07:52:31 +0900

    delete : remove project

--- StudyPjtKotlinA1/app/src/main/java/com/choco_tyranno/studypjtkotlina1/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.choco_tyranno.studypjtkotlina1
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Todo::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun todoDao(): TodoDao
-}
